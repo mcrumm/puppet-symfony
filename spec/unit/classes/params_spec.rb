8e99098d0f89633323f14799f2ca52be1f615624
Michael Crumm <mike@crumm.net>
2014-12-22 02:01:18 -0800

    Adding spec config and coverage for symfony::app::parameters

---
+++ spec/unit/classes/params_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'symfony::params', :type => :class do
+
+  context 'for osfamily RedHat' do
+    let(:facts) {{ :osfamily => 'RedHat' }}
+
+    it { should contain_class('symfony::params') }
+  end
+
+  context 'for osfamily Debian' do
+    let(:facts) {{ :osfamily => 'Debian' }}
+
+    it { should contain_class('symfony::params') }
+  end
+
+  context 'unsupported osfamily' do
+    let :facts do
+      {
+	:osfamily        => 'Windows',
+	:operatingsystem => 'Windows',
+      }
+    end
+
+    it 'should fail' do
+      expect { should contain_class('symfony::params') }.
+	to raise_error(Puppet::Error, /Unsupported platform: Windows/)
+    end
+  end
+
+end
